3b1166f2e8657c37cec75934491bbe0840477769
drAgon <rzakadrAgon@gmail.com>
2016-12-30 21:38:18 +0300

    added roboto font and material + jquery

--- index.php
+++ index.php
@@ -7,9 +7,16 @@
     <title>VAT Calc</title>
     <meta name="description" content="The VAT Calc">
     <meta name="author" content="Ziankevich">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:300,400,500,700" type="text/css">
     <link rel="stylesheet" href="css/style.css?v=1.0">
+
+    <!-- material design -->
+    <link rel="stylesheet" href="/node_modules/material-design-lite/material.min.css">
+    <script src="/node_modules/material-design-lite/material.min.js"></script>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 
     <!--[if lt IE 9]>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
@@ -63,6 +70,7 @@
             </div>
         </div>
     </div>
+
 
     <script src="js/index.js"></script>
 </body>
