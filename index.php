a6bd44224e9028b3e60a2eb53b9c6dee25fd725e
Richard <rzakadragon@gmail.com>
2016-12-30 14:25:56 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,70 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+
+    <title>VAT Calc</title>
+    <meta name="description" content="The VAT Calc">
+    <meta name="author" content="Ziankevich">
+
+    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css?v=1.0">
+
+    <!--[if lt IE 9]>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+    
+    <div class="vrapper">
+        <div class="inputbox">
+            <div class="border">
+            
+                <h2>pln vat calc v1.0</h2>
+                <div class="tableCover">
+                    <table>
+                        <tr>
+                            <td>
+                                <input type="number" id="plnWithVAT" placeholder="PLN"></input>
+                                <span>Zl</span>
+                            </td>
+                            <td>
+                                <input type="number" id="plnWithoutVAT" placeholder="PLN"></input>
+                                <span>Zl</span>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <input type="number" id="usdWithVAT" placeholder="USD"></input>    
+                                <span>$</span>    
+                            </td>
+                            <td>
+                                <input type="number" id="usdWithoutVAT" placeholder="USD"></input>  
+                                <span>$</span>       
+                            </td>
+                        </tr>
+                        <tfoot>
+                            <tr>
+                                <td>+ VAT</td>
+                                <td>- VAT</td>
+                            </tr>
+                        </tfoot>
+                    </table>
+                </div>
+
+                <br/>
+                <br/>
+                <br/>
+                <span>Zl per $</span>   
+                <input type="number" id="usdValue" value="4.226">(Zl)</input>
+                
+            </div>
+        </div>
+    </div>
+
+    <script src="js/script.js"></script>
+</body>
+</html>
+
---
+++ js/script.js
@@ -0,0 +1,41 @@
+console.info('in scripts.js')
+
+let plnWithVAT = document.getElementById('plnWithVAT')
+let plnWithoutVAT = document.getElementById('plnWithoutVAT')
+
+let usdValue = document.getElementById('usdValue')
+let usdWithVAT = document.getElementById('usdWithVAT')
+let usdWithoutVAT = document.getElementById('usdWithoutVAT')
+
+plnWithVAT.oninput = function(){ 
+    plnWithoutVAT.value = Math.round( plnWithVAT.value * 80 ) / 100
+
+    usdWithVAT.value = Math.round( plnWithVAT.value / usdValue.value )
+    usdWithoutVAT.value = Math.round( plnWithoutVAT.value / usdValue.value )
+}
+
+plnWithoutVAT.oninput = function(){ 
+    plnWithVAT.value = Math.round( plnWithoutVAT.value * 125 ) / 100
+
+    usdWithVAT.value = Math.round( plnWithVAT.value / usdValue.value )
+    usdWithoutVAT.value = Math.round( plnWithoutVAT.value / usdValue.value )
+}
+
+usdWithVAT.oninput = function(){
+    usdWithoutVAT.value = Math.round( usdWithVAT.value * 0.8 )
+
+    plnWithVAT.value = Math.round( usdWithVAT.value * usdValue.value )
+    plnWithoutVAT.value = Math.round( usdWithoutVAT.value * usdValue.value )
+}
+
+usdWithoutVAT.oninput = function(){
+    usdWithVAT.value = Math.round( usdWithoutVAT.value * 1.25 )
+
+    plnWithVAT.value = Math.round( usdWithVAT.value * usdValue.value )
+    plnWithoutVAT.value = Math.round( usdWithoutVAT.value * usdValue.value )
+}
+
+usdValue.oninput = function(){
+    usdWithVAT.value = Math.round( plnWithVAT.value / usdValue.value )
+    usdWithoutVAT.value = Math.round( plnWithoutVAT.value / usdValue.value )
+}
